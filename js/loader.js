6b0c7ce0db4bc67346bc3d2d7b58e74a3ae2f754
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2023-12-08 21:14:43 +0100

    background nero anche in fase di caricamento del wallpaper

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tempImg = new Image();
+    tempImg.src = '../img/low-quality.jpeg'; // Immagine di sfondo più leggera
+    tempImg.onload = function () {
+        const background = document.getElementById('background');
+        background.style.backgroundImage = `url('./img/Wallpaper.png')`; // Immagine di sfondo completa
+        background.style.display = 'block'; // Mostra l'immagine completa
+    };
+});
